69dcf4ee53ac7acd62e03a4e1809bfd9149cdfbb
duanxiu520 <dxc9213@163.com>
2020-09-05 15:29:38 +0800

    定时任务和mybatis配置

--- src/main/java/com/wander/cmis/CmisApplication.java
+++ src/main/java/com/wander/cmis/CmisApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan(basePackages = {"com.wander.cmis.dao"})
+@EnableScheduling
 public class CmisApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/wander/cmis/commons/QuartzConfig.java
+++ src/main/java/com/wander/cmis/commons/QuartzConfig.java
@@ -1,14 +1,10 @@
 package com.wander.cmis.commons;
 
-import com.wander.cmis.entity.UserInfo;
-import com.wander.cmis.service.UserInfoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-@EnableScheduling
 public class QuartzConfig {
 
     @Scheduled(cron = "0/5 * * * * ?")
