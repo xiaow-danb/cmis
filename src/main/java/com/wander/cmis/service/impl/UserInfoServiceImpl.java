6f6ccd58e7183ee718fe89ddf26d9ddd9ca07a66
duanxiu520 <dxc9213@163.com>
2020-09-05 16:06:07 +0800

    定时任务和mybatis配置

--- src/main/java/com/wander/cmis/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/wander/cmis/service/impl/UserInfoServiceImpl.java
@@ -4,6 +4,7 @@ import com.wander.cmis.dao.UserInfoMapper;
 import com.wander.cmis.entity.UserInfo;
 import com.wander.cmis.service.UserInfoService;
 import jdk.nashorn.internal.ir.annotations.Reference;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,10 +12,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class UserInfoServiceImpl implements UserInfoService {
 
-    @Reference
+    @Autowired
     private UserInfoMapper userInfoMapper;
     @Override
     public String getUserInfoById(String uuid) {
+        System.out.println("2222");
         return userInfoMapper.getUserName(uuid);
+//        UserInfo userInfo = userInfoMapper.selectByPrimaryKey(uuid);
+//        return userInfo.getUsername();
     }
 }
