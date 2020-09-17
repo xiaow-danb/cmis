e2f4ef74aafc925af788f4c49494bf71d3c03446
15923157990@163.com <15923157990@163.com>
2020-09-17 18:32:05 +0800

    123

--- src/main/java/com/wander/cmis/mapper/ExchangeStatusRecordMapper.java
+++
@@ -1,7 +0,0 @@
-package com.wander.cmis.mapper;
-
-import com.wander.cmis.entity.ExchangeStatusRecord;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface ExchangeStatusRecordMapper extends Mapper<ExchangeStatusRecord> {
-}
